2f8286f3dc5a3cf40d6bf7e260be1de36453bc6c
kelvinhu1992 <ihgoo2008@gmail.com>
2014-09-08 11:00:27 +0800

    Hipda工具类

---
+++ src/com/itheima/hipda/utils/HiUtils.java
@@ -0,0 +1,31 @@
+package com.itheima.hipda.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.itheima.hipda.bean.SettingHelper;
+
+public class HiUtils {
+
+	public static int getForumID(Context paramContext, long paramLong) {
+		return paramContext.getResources().getIntArray(2131165186)[((int) paramLong)];
+	}
+
+	public static String getFullUrl(String paramString) {
+		return "http://www.hi-pda.com/forum/" + paramString;
+	}
+
+	public static Boolean isAutoLoadImg(Context paramContext) {
+		if (SettingHelper.getInstance().isLoadImgOnMobileNwk()) {
+			return Boolean.valueOf(true);
+		}
+		NetworkInfo localNetworkInfo = ((ConnectivityManager) paramContext
+				.getSystemService("connectivity")).getActiveNetworkInfo();
+		if ((localNetworkInfo == null) || (!localNetworkInfo.isConnected())
+				|| (localNetworkInfo.getType() != 1)) {
+			return Boolean.valueOf(false);
+		}
+		return Boolean.valueOf(true);
+	}
+}
